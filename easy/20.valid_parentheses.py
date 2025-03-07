d619d411284a26d128e81a0aed81a7dc5376b59a
lordran <lordran@lordran.dev>
2025-03-06 22:43:42 -0600

    valid parentheses

--- easy/20.valid_parentheses.py
+++ easy/20.valid_parentheses.py
@@ -7,7 +7,29 @@
 #Open brackets must be closed in the correct order.
 # #Every close bracket has a corresponding open bracket of the same type.
 
+chars = {
+    '(' : {
+        'open' : True,
+        'close' : ')'
+    },
+    '{' : {
+        'open' : True,
+        'close' : '}'
+    },
+    '[' : {
+        'open' : True,
+        'close' : ']'
+    }
+}
 
 def is_valid(s: str) -> bool:
-    pass
-
+    for i in range(len(s) - 1) :
+        if s[i] == '(' and s[i + 1] == ')':
+            continue
+        elif s[i] == '[' and s[i + 1] == ']':
+            continue
+        elif s[i] == '{' and s[i + 1] == '}':
+            continue
+        else:
+            return False
+    return True
